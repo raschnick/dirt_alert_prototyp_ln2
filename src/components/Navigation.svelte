57b29d2db2d195a390754f1da3bbefc984e3bfca
rani <nick.rascher@hispeed.ch>
2022-05-26 18:19:14 +0200

    added final award list

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -2,13 +2,12 @@
 
 </script>
 
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-expand-sm navbar-light bg-light">
     <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="#/">Feed</a>
+                    <a class="nav-link" href="#">Feed</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#/awards">Awards</a>
